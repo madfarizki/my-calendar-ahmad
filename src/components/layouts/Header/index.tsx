0156e9d60bc47989dca9976ebf491b34893c66b9
Ahmad Alfarizki <ahmfariz24@gmail.com>
2024-04-18 12:29:58 +0700

    feat: download events
    download as json

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,6 +1,22 @@
 import { Container, Title, Menu, MenuList, MenuItem, MenuLink } from "./header.styles";
 
 function Header() {
+  const downloadEvents = () => {
+    const savedEvents = localStorage.getItem("calendarEvents");
+
+    const events = savedEvents ? JSON.parse(savedEvents) : [];
+
+    const jsonString = JSON.stringify(events, null, 2);
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = URL.createObjectURL(new Blob([jsonString], { type: "application/json" }));
+    downloadLink.download = "All Events - By Ahmad Alfarizki.json";
+
+    downloadLink.click();
+
+    URL.revokeObjectURL(downloadLink.href);
+  };
+
   return (
     <Container>
       <Title>My Personal Calendar</Title>
@@ -10,6 +26,9 @@ function Header() {
             <MenuLink href="https://madfariz.my.id" target="_blank">
               About Me
             </MenuLink>
+          </MenuItem>
+          <MenuItem>
+            <MenuLink onClick={downloadEvents}>Download Events</MenuLink>
           </MenuItem>
         </MenuList>
       </Menu>
